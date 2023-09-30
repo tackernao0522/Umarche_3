03c6203832d815bfc138e3c5211166f11acd682e
tackernao0522 <cheap_trick_magic@yahoo.co.jp>
2023-09-30 18:59:56 +0900

    管理者用及びオーナー用シーダー作成

---
+++ database/seeders/AdminTableSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
+class AdminTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('admins')->insert([
+            [
+                'name' => 'Admin',
+                'email' => 'takaki55730317@gmail.com',
+                'password' => Hash::make('password123'),
+                'created_at' => now(),
+            ]
+        ]);
+    }
+}

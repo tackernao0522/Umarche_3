9aa5f24877796e368eb4cd92deaebe16fb50b525
tackernao0522 <cheap_trick_magic@yahoo.co.jp>
2023-10-02 18:18:14 +0900

    Image Edit,Update

--- resources/views/owner/images/create.blade.php
+++ resources/views/owner/images/create.blade.php
@@ -26,7 +26,7 @@
                                 <button type="button" onclick="location.href='{{ route('owner.images.index') }}'"
                                     class="bg-gray-200 border-0 py-2 px-8 focus:outline-none hover:bg-gray-400 rounded text-lg">戻る</button>
                                 <button type="submit"
-                                    class="text-white bg-purple-500 border-0 py-2 px-8 focus:outline-none hover:bg-purple-600 rounded text-lg">更新する</button>
+                                    class="text-white bg-purple-500 border-0 py-2 px-8 focus:outline-none hover:bg-purple-600 rounded text-lg">登録する</button>
                             </div>
                         </div>
                     </form>

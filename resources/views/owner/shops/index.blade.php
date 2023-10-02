b16c08f718693f716619137486d8106eca333798
tackernao0522 <cheap_trick_magic@yahoo.co.jp>
2023-10-02 16:26:35 +0900

    Shop Editの残り

--- resources/views/owner/shops/index.blade.php
+++ resources/views/owner/shops/index.blade.php
@@ -23,11 +23,7 @@
                                     </div>
                                     <div class="text-xl">{{ $shop->name }}</div>
                                     <div>
-                                        @if (empty($shop->filename))
-                                            <img src="{{ asset('images/no_image.jpg') }}" />
-                                        @else
-                                            <img src="{{ asset('storage/shops/' . $shop->filename) }}" alt="">
-                                        @endif
+                                        <x-shop-thumbnail />
                                     </div>
                                 </div>
                             </a>

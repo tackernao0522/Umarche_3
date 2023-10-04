dac33b673e990164627a06eda93da31d937e3188
tackernao0522 <cheap_trick_magic@yahoo.co.jp>
2023-10-04 13:31:52 +0900

    商品一覧の準備/商品一覧ビュー側の調整

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\ComponentTestController;
 use App\Http\Controllers\LifeCycleTestController;
+use App\Http\Controllers\User\ItemController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,6 +18,10 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('user.welcome');
+});
+
+Route::middleware('auth:users')->group(function () {
+    Route::get('/', [ItemController::class, 'index'])->name('items.index');
 });
 
 Route::get('/dashboard', function () {

7d44bf7db6579ce0f25e5a578c4dc04c2afa193e
tackernao0522 <cheap_trick_magic@yahoo.co.jp>
2023-10-06 16:09:55 +0900

    ログインセッションの修正

--- config/session.php
+++ config/session.php
@@ -128,9 +128,16 @@ return [
 
     'cookie' => env(
         'SESSION_COOKIE',
-        Str::slug(env('APP_NAME', 'laravel'), '_').'_session'
+        Str::slug(env('APP_NAME', 'laravel'), '_') . '_session'
     ),
-
+    'cookie_owner' => env(
+        'SESSION_COOKIE_OWNER',
+        Str::slug(env('APP_NAME', 'laravel'), '_') . '_session_owner'
+    ),
+    'cookie_admin' => env(
+        'SESSION_COOKIE_ADMIN',
+        Str::slug(env('APP_NAME', 'laravel'), '_') . '_session_admin'
+    ),
     /*
     |--------------------------------------------------------------------------
     | Session Cookie Path

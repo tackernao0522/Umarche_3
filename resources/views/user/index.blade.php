c3c1258f1d793edfd9fbe70a52fa534eb188a1cc
tackernao0522 <cheap_trick_magic@yahoo.co.jp>
2023-10-06 12:37:16 +0900

    検索フォーム ビュー側調整

--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -1,11 +1,33 @@
 <x-app-layout>
     <x-slot name="header">
-        <div class="flex justify-between items-center">
-            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-                {{ __('商品一覧') }}
-            </h2>
-            <div>
-                <form method="GET" action="{{ route('user.items.index') }}">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('商品一覧') }}
+        </h2>
+        <div>
+            <form method="GET" action="{{ route('user.items.index') }}">
+                <div class="lg:flex lg:justify-around">
+                    <div class="lg:flex items-center">
+                        <select name="category" class="mb-2 lg:mb-0 lg:mr-2">
+                            <option value="0">全て</option>
+                            @foreach ($categories as $category)
+                                <optgroup label="{{ $category->name }}">
+                                    @foreach ($category->secondaries as $secondary)
+                                        <option value="{{ $secondary->id }}"
+                                            {{ old('category') == $secondary->id ? 'selected' : '' }}>
+                                            {{ $secondary->name }}
+                                        </option>
+                                    @endforeach
+                                </optgroup>
+                            @endforeach
+                        </select>
+                        <div class="flex space-x-2 items-center">
+                            <div><input name="keyword" class="border border-gray-500 py-2" placeholder="キーワードを入力"></div>
+                            <div>
+                                <button
+                                    class="ml-auto text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded">検索する</button>
+                            </div>
+                        </div>
+                    </div>
                     <div class="flex">
                         <div>
                             <span class="text-sm">表示順</span><br>
@@ -39,9 +61,11 @@
                             </select>
                         </div>
                     </div>
-                </form>
-            </div>
+                </div>
+            </form>
         </div>
+
+
     </x-slot>
 
     <div class="py-12">

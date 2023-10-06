a274a2c78083306e01e263f4db635b800661d5bb
tackernao0522 <cheap_trick_magic@yahoo.co.jp>
2023-10-06 14:10:03 +0900

    CartServiceの作成

--- app/Http/Controllers/User/CartController.php
+++ app/Http/Controllers/User/CartController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Models\Cart;
 use App\Models\Stock;
 use App\Models\User;
+use App\Services\CartService;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
@@ -59,6 +60,9 @@ class CartController extends Controller
 
     public function checkout()
     {
+        $items = Cart::where('user_id', Auth::id())->get();
+        $products = CartService::getItemsCart($items);
+
         $user = User::findOrFail(Auth::id());
         $products = $user->products;
         $stripe = new \Stripe\StripeClient(env('STRIPE_SECRET_KEY'));
